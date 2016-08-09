ebd6ec8aaabe5b50c53a2922b788bdcae82fd847
Richard Eames <reames@asymmetricventures.com>
2016-08-09 12:33:27 -0600

    bf for django 1.10

--- aldjemy/middleware.py
+++ aldjemy/middleware.py
@@ -2,6 +2,16 @@
 from aldjemy.orm import close_session
 
 class CloseSaSession:
+	def __init__(self, get_response = None):
+		self.get_response = get_response
+	
+	def __call__(self, request):
+		response = None
+		if self.get_response is not None:
+			response = self.get_response(request)
+		return self.process_response(request, response)
+		
+	
 	def _close(self):
 		close_session()
 	
